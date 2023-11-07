eb293cedaa519a4533ea1812d47fc9c4c04abc78
perseus-algol <perseus.algol2@gmail.com>
2023-11-07 19:41:16 +0300

    refactoring

--- src/redux-tui/render.ts
+++ src/redux-tui/render.ts
@@ -1,4 +1,4 @@
-import { Dispatch, createAction } from "@reduxjs/toolkit";
+import { Dispatch, createAction, createAsyncThunk, freeze } from "@reduxjs/toolkit";
 import { Prompt } from "./types/interactions";
 import cloneDeep from 'lodash.clonedeep';
 import enquirer from 'enquirer';
@@ -6,29 +6,38 @@ import enquirer from 'enquirer';
 const { Select, Input, Separator } = (enquirer as any);
 const { prompt } = enquirer;
 
+const selectItems = {
+  separator: {
+    name: 'separator',
+    role: 'separator'
+  },
+  back: {
+    name: 'back',
+    message: 'Back'
+  }
+};
+
 const handleInteraction = async (dispatch: Dispatch, interaction?: Prompt) => {
   if (!interaction) {
     return;
   }
-  
-  if (interaction.type === 'sequence') {
-    const a: any = await prompt(cloneDeep(interaction.sequence) as any);
-    const action = createAction<any>('sequence');
-    dispatch(action(a));
-  } else {
-    if (interaction.type === 'select') {
-      interaction.choices.push({
-        name: 'separator',
-        role: 'separator'
-      }, {
-        name: 'back',
-        message: 'Back'
-      })
-    }
-    const a: any = await prompt(cloneDeep(interaction) as any);
-    const action = createAction<any>(a[interaction.name]);
-    dispatch(action(a));
+
+  if (interaction.type === 'select') {
+    interaction.choices.push(selectItems.separator, selectItems.back);
   }
+
+  const whatToPromt = interaction.type === 'sequence'
+    ? interaction.sequence
+    : interaction;
+
+  const a: any = await prompt(whatToPromt as any);
+  
+  const actionType = interaction.type === 'sequence'
+    ? interaction.type
+    : a[interaction.name];
+
+  const action = createAction<any>(actionType);
+  dispatch(action(a));
 }
 
 export const render = async (p: {
