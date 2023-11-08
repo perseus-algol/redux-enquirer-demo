a441b657c63f8e55167901b7b21cc2d577d97678
perseus-algol <perseus.algol2@gmail.com>
2023-11-08 15:54:37 +0300

    Create lib functions

--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,5 @@
 import { faker } from "@faker-js/faker";
+import { mockQuestions } from "./mock-data/questions";
 
 export type Question = {
   questionID: string,
@@ -17,34 +18,75 @@ export type QuestionCreateResult = {
   questionID: string,
 }
 
-export type Tx = { tx: string }
+type Tx = { tx: string }
 
 const delayMs = 1000;
 
 const txHash = () => faker.string.hexadecimal({length: 64, prefix: ''});
 
-export const createTxPromise = (): Promise<Tx> => new Promise((res, _) => {
+const txPromise: Promise<Tx> = new Promise((res, _) => {
   setTimeout(() => res({ tx: txHash() }), delayMs);
 })
 
-export const createVoidPromise = (): Promise<void> => new Promise((res, _) => {
+const voidPromise: Promise<void> = new Promise((res, _) => {
   setTimeout(() => res(), delayMs);
 })
 
-// export const oracleCreate = async (): Promise<Tx> => {
-//   return createTxPromise();
-// }
+// Oracle
 
-// export const oracleResolve = async (oracle: string, outcome: OutcomeResolution): Promise<Tx> => {
-//   return createTxPromise();
-// }
+export const oracleCreate = txPromise;
 
-// export const updateOutcomeInDb = async (questionID: string, outcome: OutcomeResolution): Promise<void> => {
-//   return createVoidPromise();
-// }
+export const oracleResolve = async (oracle: string, outcome: OutcomeResolution): Promise<Tx> => {
+  console.log('');
+  return txPromise;
+}
 
-// export const questionCreate = async (question: Question): Promise<QuestionCreateResult> => {
-//   return new Promise((res, _) => {
-//     setTimeout(() => res({ tx: txHash(), questionID: question.questionID }), delayMs);
-//   })
-// }
+export const updateOutcomeInDb = async (questionID: string, outcome: OutcomeResolution): Promise<void> => {
+  return voidPromise;
+}
+
+export const oracleClose = async (oracle: string) => {
+  return txPromise;
+}
+
+// Questions
+
+export const questionCreate = async (question: Question): Promise<QuestionCreateResult> => {
+  return new Promise((res, _) => {
+    setTimeout(() => res({ tx: txHash(), questionID: question.questionID }), delayMs);
+  })
+}
+
+export const questionDelete = async (questionID: string) => {
+  return voidPromise;
+}
+
+export const removeWaitingOperations = async (questionID: string) => {
+  return voidPromise;
+}
+
+export const getQuestionById = async (questionID: string): Promise<Question> => {
+  return new Promise((res, _) => {
+    setTimeout(() => {
+      const result = mockQuestions.find(i => i.questionID === questionID);
+      if (!result) {
+        throw new Error("Question not found");
+      }
+      return res(result);
+    }, delayMs);
+  });
+}
+
+export const listQuestions = async (filter: 'all' | 'open' | 'completed' = 'all'): Promise<Question[]> => {
+  return new Promise((res, _) => {
+    setTimeout(() => {
+      const result = filter === 'all' 
+        ? mockQuestions
+        : filter === 'open'
+          ? mockQuestions.filter(i => i.outcome === 'Undefined')
+          : mockQuestions.filter(i => i.outcome !== 'Undefined');
+
+      return res(result);
+    }, delayMs);
+  });
+}
